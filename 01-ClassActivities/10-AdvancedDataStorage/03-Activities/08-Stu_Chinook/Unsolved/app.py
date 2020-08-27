1b5fdafa83d6d1265e9d2b97b6fe0205be2c17f5
Pleon <patrickedwardleon@gmail.com>
2020-08-26 19:37:38 -0700

    Adding Web to Activities

--- 01-ClassActivities/10-AdvancedDataStorage/03-Activities/08-Stu_Chinook/Unsolved/app.py
+++ 01-ClassActivities/10-AdvancedDataStorage/03-Activities/08-Stu_Chinook/Unsolved/app.py
@@ -34,7 +34,50 @@ app = Flask(__name__)
 
 ## YOUR CODE HERE ########
 
+@app.route("/")
+def welcome():
+    # <a href='/api/v1.0/justice-league'> http://127.0.0.1:5000/api/v1.0/justice-league </a>
+    return "Hello"
 
+@app.route("/billing_countries")
+def billing_countries():
+    session = Session(engine)
+    results = session.query(Invoices.BillingCountry).group_by(Invoices.BillingCountry).all()
+    session.close()
+
+    all_results = list(np.ravel(results))
+    return jsonify(all_results)
+
+@app.route("/invoices_total")
+def invoices_total():
+    session = Session(engine)
+    results = session.query(Invoices.BillingCountry, func.sum(Invoices.Total)).group_by(Invoices.BillingCountry).order_by(func.sum(Invoices.Total).desc()).all()
+    session.close()
+    
+    all_results = []
+    for x in results:
+        item_dict = {}
+        item_dict["country"] = x[0]
+        item_dict["total"] = float(x[1])
+        all_results.append(item_dict)
+    return jsonify(all_results)
+    
+@app.route("/postal_codes")
+def postal_codes():
+    session = Session(engine)
+    results = session.query(Invoices.BillingPostalCode).filter(Invoices.BillingCountry == 'USA').group_by(Invoices.BillingPostalCode).all()
+    session.close()
+
+    results = list(np.ravel(results))
+    return jsonify(results)
+
+# @app.route("/agg_total")
+# def agg_total():
+#     return 
+
+# @app.route("/item_totals")
+# def item_totals():
+#     return 
 
 if __name__ == '__main__':
     app.run(debug=True)
